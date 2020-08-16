ee497a9ac107fbb7176bcbcb86be5d9a25f7729b
Felix Tito <56932963+felixtitonina@users.noreply.github.com>
2020-08-16 02:21:55 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const routes = require('./routers/routes');
 
 const app = express(); // crio a aplicação com a função express
 
-mongoose.connect('mongodb+srv://felixtitonina:SU54506083p0@cluster0-ija1l.mongodb.net/casa?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://@cluster0-ija1l.mongodb.net/casa?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
